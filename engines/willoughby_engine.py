bdb5537e00a3420eb67fcc87e320ab99bf61683e
omar-kallas <ok762@nyu.edu>
2023-01-11 15:26:50 +0400

    Delete engines directory

--- engines/willoughby_engine.py
+++
@@ -1,9 +0,0 @@
-from engine import Engine
-
-class WilloughbyEngine(Engine):
-    def __init__(self, current_mileage, last_service_mileage):
-        self.current_mileage = current_mileage
-        self.last_service_mileage = last_service_mileage
-
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 60000
